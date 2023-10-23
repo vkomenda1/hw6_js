6e6271b04d6493973d2642c2082530659f9d70fd
vkomenda1 <wkomenda@gmail.com>
2023-10-23 16:56:11 +0300

    default file

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  watchForFileChanges: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
